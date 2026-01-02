71004500a2da27d0f1c6a5b007732fc16d85f280
vtf6259 <125233907+vtf6259@users.noreply.github.com>
2026-01-02 13:50:00 -0700

    idk what to put here

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+}
+
+function sleep(ms) {
+  setTimeout(null, ms)
+}
 const audioCtx = new AudioContext();
 if (navigator.mediaDevices) {
   navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
-    const microphone = audioCtx.createMediaStreamSource(stream);
-    // `microphone` can now act like any other AudioNode
-    console.log(microphone)
+    const mediaRecorder = new MediaRecorder(stream);
+    let chunks = [];
+    mediaRecorder.ondataavailable = (e) => {
+      chunks.push(e.data);
+    };
+    mediaRecorder.onstop = (e) => {
+      const audio = document.createElement("audio")
+      const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+      chunks = [];
+      const audioURL = window.URL.createObjectURL(blob);
+      audio.src = audioURL;
+  };
+    //mediaRecorder.start()
+    //await sleep(2000)
+    //mediaRecorder.stop()
+    
   }).catch((err) => {
     // browser unable to access microphone
     // (check to see if microphone is attached)
