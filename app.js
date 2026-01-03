cc4108c1347515928a43f27a17b93cbd64eaf54a
vtf6259 <125233907+vtf6259@users.noreply.github.com>
2026-01-02 18:25:43 -0700

    READY

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ if (navigator.mediaDevices) {
     const startButton = document.getElementById("start")
     const recordingtext = document.getElementById("recordingtext")
     const blobLink = document.getElementById("bloblink")
+    let audioblob = ""
+    const playButton = document.getElementById("play")
     const mediaRecorder = new MediaRecorder(stream);
     let chunks = [];
     mediaRecorder.ondataavailable = (e) => {
@@ -23,7 +25,7 @@ if (navigator.mediaDevices) {
       chunks = []
       const audioURL = window.URL.createObjectURL(blob)
       console.log(audioURL)
-      blobLink.href = audioURL
+      audioblob = audioURL
       //window.open(audioURL, '_blank').focus()
       
       //audio.src = audioURL;
@@ -41,7 +43,27 @@ if (navigator.mediaDevices) {
       stopButton.disabled = true
       recordingtext.innerText = "Not recording"
     })
+    playButton.addEventListener("click", async (event) =>{
+      const ctx = new (window.AudioContext || window.webkitAudioContext)();
+      const resp = await fetch(audioblob);
+      const arrayBuffer = await resp.arrayBuffer();
+      const audioBuf = await ctx.decodeAudioData(arrayBuffer);
+
+      // reverse each channel in-place
+      for (let c = 0; c < audioBuf.numberOfChannels; c++) {
+        const channel = audioBuf.getChannelData(c);
+        for (let i = 0, j = channel.length - 1; i < j; i++, j--) {
+          const t = channel[i];
+          channel[i] = channel[j];
+          channel[j] = t;
+        }
+      }
     
+      const voice = ctx.createBufferSource();
+      voice.buffer = audioBuf;
+      voice.connect(ctx.destination);
+      voice.start()
+    })
   }).catch((err) => {
     // browser unable to access microphone
     // (check to see if microphone is attached)
